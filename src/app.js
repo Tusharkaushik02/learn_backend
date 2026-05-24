d1efcd947c34cb1b83e4fb9455ff3926ca65f487
Tushar kaushik <tusharkaushik520@gmail.com>
2026-05-24 11:28:19 +0530

    delete logic

--- src/app.js
+++ src/app.js
@@ -2,20 +2,54 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+const notes =[];
 
 app.post('/data', (req, res) => {
     const data =req.body;
     console.log('Received data:', data);
-    res.json({ message: 'Data received', data });
+    notes.push(data);
+    res.status(201).json({
+        message: 'Data received',
+        data
+    });
 });
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/about', (req, res) => {
-    res.send('This is the about page');
+app.get('/data', (req, res) => {
+    res.status(200).json({
+        message: 'This is the data page',
+        notes:notes
+    });
 });
 
+app.delete('/data/:id', (req,res)=>{
+    const id =Number(req.params.id);
+    let found =false;
+    notes.forEach(innerArray=>{
+        const index=innerArray.findIndex(note=>note.id===id);
+    
+    if(index!==-1){
+        innerArray.splice(index,1);
+        found = true;
+        }
+    });
+    
+    if(found){
+
+        res.status(200).json({
+            message: 'Data deleted successfully',
+            notes
+        });
+
+    } else {
+
+        res.status(404).json({
+            message: 'Data not found'
+        });
+    }
+})
 
 module.exports = app
